397fb3f9d72a491223a12c868ecfca2101a85904
Amit Choudhary <choudharyamit211998@gmail.com>
2026-05-19 23:58:55 +0530

    first commit

---
+++ test/test_calculator.py
@@ -0,0 +1,16 @@
+from app.calculator import add, subtract, multiply, divide
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def subtract():
+    assert(5, 2) == 3
+
+
+def multiply():
+    assert multiply(2, 4) == 8
+
+
+def test_divide():
+    assert divide(10, 2) == 5          
